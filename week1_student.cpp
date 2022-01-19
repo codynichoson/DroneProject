fa0d3f7500841abdb0f62878238f0f6c2dfacf8e
Cody Nichoson <62906322+codynichoson@users.noreply.github.com>
2022-01-19 16:17:37 -0600

    Added week1_student file

---
+++ week1_student.cpp
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include <time.h>
+#include <math.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <signal.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <curses.h>
+
+//gcc -o week1 week_1.cpp -lwiringPi -lncurses -lm
+
+#define frequency 25000000.0
+#define CONFIG           0x1A
+#define SMPLRT_DIV       0x19
+#define GYRO_CONFIG      0x1B
+#define ACCEL_CONFIG     0x1C
+#define ACCEL_CONFIG2    0x1D
+#define USER_CTRL        0x6A  // Bit 7 enable DMP, bit 3 reset DMP
+#define PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
+#define PWR_MGMT_2       0x6C
+
+
+enum Ascale {
+  AFS_2G = 0,
+  AFS_4G,
+  AFS_8G,
+  AFS_16G
+};
+ 
+enum Gscale {
+  GFS_250DPS = 0,
+  GFS_500DPS,
+  GFS_1000DPS,
+  GFS_2000DPS
+};
+ 
+int setup_imu();
+void calibrate_imu();      
+void read_imu();    
+void update_filter();
+
+//global variables
+int imu;
+float x_gyro_calibration=0;
+float y_gyro_calibration=0;
+float z_gyro_calibration=0;
+float roll_calibration=0;
+float pitch_calibration=0;
+float accel_z_calibration=0;
+float imu_data[6]; //gyro xyz, accel xyz,  pitch roll
+long time_curr;
+long time_prev;
+struct timespec te;
+float yaw=0;
+float pitch_angle=0;
+float roll_angle=0;
+
+ 
+int main (int argc, char *argv[])
+{
+
+    setup_imu();
+    calibrate_imu();
+    
+    while(1)
+    {
+      read_imu();      
+      update_filter();    
+    }
+}
+
+void calibrate_imu()
+{
+  float roll_tot = 0, pitch_tot = 0;
+  float z_accel = 0;
+  float x_gyro = 0, y_gyro = 0, z_gyro = 0;
+  for (int i = 0; i < 1000; i++){
+    read_imu();
+    roll_tot = roll_tot + imu_data[7];
+    pitch_tot = pitch_tot + imu_data[6];
+    // x_accel = x_accel + imu_data[3];
+    // y_accel = y_accel + imu_data[4];
+    z_accel = z_accel + imu_data[5];
+    x_gyro = x_gyro + imu_data[0];
+    y_gyro = y_gyro + imu_data[1];
+    z_gyro = z_gyro + imu_data[2];
+  }
+
+  x_gyro_calibration= x_gyro/-1000.0;
+  y_gyro_calibration= y_gyro/-1000.0;
+  z_gyro_calibration= z_gyro/-1000.0;
+  roll_calibration= roll_tot/-1000.0;
+  pitch_calibration= pitch_tot/-1000.0;
+  accel_z_calibration= z_accel/-1000.0;
+  
+printf("calibration complete\nx_gyro: %f\ny_gyro: %f\nz_gyro: %f\nroll: %f\npitch: %f\nz_accel: %f\n\r",x_gyro_calibration,y_gyro_calibration,z_gyro_calibration,roll_calibration,pitch_calibration,accel_z_calibration);
+
+
+}
+
+void read_imu()
+{
+  int address=59;//todo: set address value for accel x value 
+  float ax=0;
+  float az=0;
+  float ay=0; 
+  int vh,vl;
+  
+  //read in data
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  //convert 2 complement
+  int vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+  imu_data[3]=((float)vw/32767.0)*2.0;//  todo: convert vw from raw values to "g's"
+  
+  
+  address=61;//todo: set address value for accel y value
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+  imu_data[4]=((float)vw/32767.0)*2.0;//Todo: convert vw from raw values to "g's"
+  
+  
+  address=63;//todo: set addres value for accel z value;
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+  imu_data[5]= accel_z_calibration + ((float)vw/32767.0)*2.0;//todo: convert vw from raw values to g's
+  
+  
+  address=67;//todo: set addres value for gyro x value;
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+  imu_data[0]=x_gyro_calibration+((float)vw/32767.0)*500;////todo: convert vw from raw values to degrees/second
+  
+  address=69;//todo: set addres value for gyro y value;
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+ imu_data[1]=y_gyro_calibration+((float)vw/32767.0)*500;////todo: convert vw from raw values to degrees/second
+  
+  address=71;////todo: set addres value for gyro z value;
+  vh=wiringPiI2CReadReg8(imu,address);
+  vl=wiringPiI2CReadReg8(imu,address+1);
+  vw=(((vh<<8)&0xff00)|(vl&0x00ff))&0xffff;
+  if(vw>0x8000)
+  {
+    vw=vw ^ 0xffff;
+    vw=-vw-1;
+  }          
+  imu_data[2]=z_gyro_calibration+((float)vw/32767.0)*500;////todo: convert vw from raw values to degrees/second
+  
+  // pitch_angle = pitch_calibration + atan2(-imu_data[4],-imu_data[5]-accel_z_calibration)*180/3.14; //pitch
+  // roll_angle = roll_calibration + atan2(imu_data[3],-imu_data[5]-accel_z_calibration)*180/3.14; //roll
+
+  pitch_angle = atan2(-imu_data[4],-imu_data[5]-accel_z_calibration)*180/3.14; //pitch
+  roll_angle = atan2(imu_data[3],-imu_data[5]-accel_z_calibration)*180/3.14; //roll
+
+  printf("Gyro   X:%6.2f   Y:%6.2f   Z:%6.2f\n", imu_data[0], imu_data[1], imu_data[2]);
+  printf("Accel  X:%6.2f   Y:%6.2f   Z:%6.2f\n", imu_data[3], imu_data[4], imu_data[5]);
+  printf("Roll:%6.2f   Pitch:%6.2f  \n\n", roll_angle, pitch_angle);
+
+
+}
+
+void update_filter()
+{
+
+  //get current time in nanoseconds
+  timespec_get(&te,TIME_UTC);
+  time_curr=te.tv_nsec;
+  //compute time since last execution
+  float imu_diff=time_curr-time_prev;           
+  
+  //check for rollover
+  if(imu_diff<=0)
+  {
+    imu_diff+=1000000000;
+  }
+  //convert to seconds
+  imu_diff=imu_diff/1000000000;
+  time_prev=time_curr;
+  
+  //comp. filter for roll, pitch here: 
+}
+
+
+int setup_imu()
+{
+  wiringPiSetup ();
+  
+  
+  //setup imu on I2C
+  imu=wiringPiI2CSetup (0x68) ; //accel/gyro address
+  
+  if(imu==-1)
+  {
+    printf("-----cant connect to I2C device %d --------\n",imu);
+    return -1;
+  }
+  else
+  {
+  
+    printf("connected to i2c device %d\n",imu);
+    printf("imu who am i is %d \n",wiringPiI2CReadReg8(imu,0x75));
+    
+    uint8_t Ascale = AFS_2G;     // AFS_2G, AFS_4G, AFS_8G, AFS_16G
+    uint8_t Gscale = GFS_500DPS; // GFS_250DPS, GFS_500DPS, GFS_1000DPS, GFS_2000DPS
+    
+    
+    //init imu
+    wiringPiI2CWriteReg8(imu,PWR_MGMT_1, 0x00);
+    printf("                    \n\r");
+    wiringPiI2CWriteReg8(imu,PWR_MGMT_1, 0x01);
+    wiringPiI2CWriteReg8(imu, CONFIG, 0x00);  
+    wiringPiI2CWriteReg8(imu, SMPLRT_DIV, 0x00); //0x04        
+    int c=wiringPiI2CReadReg8(imu,  GYRO_CONFIG);
+    wiringPiI2CWriteReg8(imu,  GYRO_CONFIG, c & ~0xE0);
+    wiringPiI2CWriteReg8(imu, GYRO_CONFIG, c & ~0x18);
+    wiringPiI2CWriteReg8(imu, GYRO_CONFIG, c | Gscale << 3);       
+    c=wiringPiI2CReadReg8(imu, ACCEL_CONFIG);
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c & ~0xE0); // Clear self-test bits [7:5] 
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c & ~0x18); // Clear AFS bits [4:3]
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG, c | Ascale << 3);      
+    c=wiringPiI2CReadReg8(imu, ACCEL_CONFIG2);         
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG2, c & ~0x0F); //
+    wiringPiI2CWriteReg8(imu,  ACCEL_CONFIG2,  c | 0x00);
+  }
+  return 0;
+}
+
+
